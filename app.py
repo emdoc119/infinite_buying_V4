0945f25315d47960b542c129c5198ebce63896e3
Choo <choo@Choo-Mac-mini.local>
2026-07-03 11:42:20 +0900

    Auto-update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 from decimal import Decimal
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 from typing import Optional, Dict
 import yfinance as yf
 
